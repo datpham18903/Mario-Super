31d1e10f273894f26bdc6d75d2466d71a3029655
HungCuong862003 <cuongphan444888@gmail.com>
2023-12-21 23:27:58 +0700

    Restart fixed

---
+++ src/manager/GameStatus.java
@@ -0,0 +1,13 @@
+package manager;
+
+public enum GameStatus {
+    GAME_OVER,
+    PAUSED,
+    RUNNING,
+    START_SCREEN,
+    MAP_SELECTION,
+    HELP_SCREEN,
+    MISSION_PASSED,
+    ABOUT_SCREEN,
+    VICTORY
+}
